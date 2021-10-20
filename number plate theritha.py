58b94719e1ea84a060d7bb23032b30f12d403467
Juju26 <atshayakumarr26@gmail.com>
2021-10-20 22:45:22 +0530

    anpr py

---
+++ number plate theritha.py
@@ -0,0 +1,15 @@
+import easyocr
+import os
+import glob
+
+#filenames='C:/Users/Asus/OneDrive/Desktop/ANPR scribble pad/Anbe car uh/bmw white front.png'
+filenames='C:/Users/Asus/OneDrive/Desktop/ANPR scribble pad/Anbe car uh/tata sus.jpeg'
+with open(filenames,'r') as fi:
+    res_list=[]
+    read=easyocr.Reader(['en'])
+    result=read.readtext(filenames)
+    for i in range(len(result)):    
+        p=[]
+        p.append(result[i][1])
+        res_list.append(''.join(i for i in p))
+    print(res_list)
